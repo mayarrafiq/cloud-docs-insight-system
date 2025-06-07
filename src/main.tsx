77c706fbce1bc3c95551a9c88d5686a31ba993d8
mayarrafiq <mayarrafiq56@gmail.com>
2025-06-07 20:58:42 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import 'pdfjs-dist/web/pdf_viewer.css';
 createRoot(document.getElementById("root")!).render(<App />);
